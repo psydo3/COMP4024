f33e19bddd88fda296738948e31c482e0d6d1488
Febin Thomas <thfebin@gmail.com>
2024-02-15 01:57:29 +0000

    User name input is displayed on new scene

---
+++ Number Ninja/Assets/displayUserName.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+
+public class displayUserName : MonoBehaviour
+{
+    public TextMeshProUGUI displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (displayText != null)
+        {
+            // Set the text of a UI Text component to the userInput
+            displayText.text = "Hi "+startMain.getUserName()+"!";
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Number Ninja/Assets/startMain.cs
+++ Number Ninja/Assets/startMain.cs
@@ -6,6 +6,8 @@ using TMPro;
 
 public class startMain : MonoBehaviour
 {
+    public TMP_InputField inputField; // Reference to the input field
+    public static string userName; // Variable to store the input
 
     public void LoadScene()
     {
@@ -14,7 +16,27 @@ public class startMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (inputField != null)
+        {
+            Debug.Log("from start  - test");
+            // Add a listener to call the OnInputChange method whenever the text changes
+            inputField.onValueChanged.AddListener(delegate { OnInputChange(inputField.text); });
+        }
+        else
+        {
+            Debug.Log("from else  - test");
+        }
+    }
+
+    public void OnInputChange(string text)
+    {
+        userName = text;
+        Debug.Log("User Input: " + userName);
+    }
+
+    public static string getUserName()
+    {
+        return userName;
     }
 
     // Update is called once per frame
