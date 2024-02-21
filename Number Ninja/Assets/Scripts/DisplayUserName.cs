ac394cc6940fd58d3c67cfb2245b8d49d8dec8d0
Febin Thomas <thfebin@gmail.com>
2024-02-21 13:05:40 +0000

    Linked score var with score field in ui through QuizManager

--- Number Ninja/Assets/Scripts/DisplayUserName.cs
+++ Number Ninja/Assets/Scripts/DisplayUserName.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class displayUserName : MonoBehaviour
 {
     public TextMeshProUGUI displayText;
-    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +13,6 @@ public class displayUserName : MonoBehaviour
         {
             // Set the text of a UI Text component to the userInput
             displayText.text = "Hi "+StartMain.getUserName()+"!";
-
-            scoreText.text = QuizManager.userScore.ToString();
         }
     }
 
--- Number Ninja/Assets/Scripts/QuizManager.cs
+++ Number Ninja/Assets/Scripts/QuizManager.cs
@@ -1,25 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class QuizManager : MonoBehaviour
 {
     public QuizData quizData; // Holds the loaded quiz data
+    public TextMeshProUGUI scoreText;
     //Create a variable to track the user score
-    public static int userScore;
+    int userScore;
     
     //public TextMeshProUGUI displayScore;
 
     void Start()
     {
-        //Call the appropriate function to load the quiz data
-
         Debug.Log("start called");
+        //Call the appropriate function to load the quiz data
+        loadQuizData();
+        evaluateAnswer(0);
     }
 
     void loadQuizData()
     {
         TextAsset quizJson = Resources.Load<TextAsset>("quizData"); // Load the JSON file from Resources
         quizData = JsonUtility.FromJson<QuizData>(quizJson.text); // Deserialize the JSON into the QuizData object
-
         //Add a log
         Debug.Log("quiz data loaded");
     }
@@ -27,9 +29,16 @@ public class QuizManager : MonoBehaviour
     void evaluateAnswer(int answer)
     {
         //Add a log
+        Debug.Log("Evaluate answer called");
 
         //Compare the answer the user has clicked with the answer from quiz data
-        
+        userScore = 1;
+        if (scoreText != null)
+        {
+            // Set the text of a UI Text component to the userInput
+            scoreText.text = userScore.ToString();
+        }
+
         //Increase the score if correct
     }
 
